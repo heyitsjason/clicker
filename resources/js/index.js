7018bec136b6ef757c8b2a5b8c6a6c355b6f868c
Wang <jaswang@amazon.com>
2016-10-02 10:53:47 -0700

    cleanup

--- resources/js/index.js
+++
@@ -1,9 +0,0 @@
-function run() {
-    state.regions || (state.regions = {});
-
-    state.regions.header = document.getElementsByClassName('header-region')[0];
-    state.regions.content = document.getElementsByClassName('content-region')[0];
-    state.regions.footer = document.getElementsByClassName('footer-region')[0];
-
-    route();
-}
--- resources/js/router.js
+++ resources/js/router.js
@@ -2,29 +2,37 @@ const DEFAULT_FRAGMENT = '#home';
 
 window.onhashchange = route;
 
+function navigate(route) {
+    window.location.hash = "#" + route;
+}
+
 function route() {
     var fragment = window.location.hash || DEFAULT_FRAGMENT;
 
     routes[fragment] ? routes[fragment]() : routes[DEFAULT_FRAGMENT]();
 };
 
+function loadRegion(uri, region, callback) {
+    ajax.get(uri, function(response) {
+        region.innerHTML = response;
+        callback && callback();
+    });
+};
+
 var routes = {
     '#home': function() {
-        state.regions.header.innerHTML = 'header';
-        // state.regions.content.innerHTML = 'content';
-        state.regions.footer.innerHTML = 'footer';
-
-        ajax.get('resources/test.html', function(response) {
-            state.regions.content.innerHTML = response;
-        });
+        state.regions.content.innerHTML = 'Home';
     },
-    '#home2': function() {
-        state.regions.header.innerHTML = 'header';
-        // state.regions.content.innerHTML = 'content';
-        state.regions.footer.innerHTML = 'footer';
-
-        ajax.get('resources/test2.html', function(response) {
-            state.regions.content.innerHTML = response;
-        });
+    '#train': function() {
+        state.regions.content.innerHTML = 'Train';
+    },
+    '#defend': function() {
+        state.regions.content.innerHTML = 'Defend';
+    },
+    '#stats': function() {
+        state.regions.content.innerHTML = 'Stats';
+    },
+    '#login': function() {
+        state.regions.content.innerHTML = 'Login';
     }
 }
